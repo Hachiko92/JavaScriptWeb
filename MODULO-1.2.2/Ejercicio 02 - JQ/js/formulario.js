3c4f566d27d6c2c93e955486c6c04af971ee685d
Hachiko92 <eleonora.franchini@tajamar365.com>
2017-05-23 21:37:35 +0200

    Ejercicio

--- MODULO-1.2.2/Ejercicio 02 - JQ/js/formulario.js
+++ MODULO-1.2.2/Ejercicio 02 - JQ/js/formulario.js
@@ -1,36 +1,28 @@
 class Formulario {
     constructor (pNombre, pCurso) {
         this.oDOM = {
-            fNombre: document.querySelector("#first_name"),
-            btnBorrar: document.querySelector("#btnBorrar"),
-            spanNombre: document.querySelector("#nombre"),
-            spanCurso: document.querySelector("#curso")
+            fNombre: $("#first_name"),
+            btnBorrar: $("#btnBorrar"),
+            spanNombre: $("#nombre"),
+            spanCurso: $("#curso")
         };//fin del objeto oDOM
         
-        this.oDOM.fNombre.value = pNombre;
-        this.oDOM.spanNombre.innerHTML = this.oDOM.fNombre.value;
-        this.oDOM.spanCurso.innerHTML = pCurso;
-
-        // como en main.js creo un New Formulario, cuando se crea en el constructor puedo executar la funcion
-        // controller, pero es mejor ponerla fuera aunque es la misma cosa.
-        //this.controller();
-    }
+        // pongo [0] porque todavia todo el codigo no está escrito en JQuery 
+        this.oDOM.fNombre[0].value = pNombre;
+        this.oDOM.spanNombre[0].innerHTML = this.oDOM.fNombre[0].value;
+        this.oDOM.spanCurso[0].innerHTML = pCurso;
     controller () {
 
-        // en espera de eventos: 
-        // tengo que poner .bind(this) porque el navegador considera "this" come el suyo y no encuentra
-        // las variables, por esto pongo la funcion .bind(this) para hacer si que entienda que es ESTO this
-        this.oDOM.fNombre.addEventListener("keyup", this.actualizarVista.bind(this));
-        this.oDOM.btnBorrar.addEventListener("click", this.actualizarVista.bind(this));
+        this.oDOM.fNombre.on("keyup", this.actualizarVista.bind(this));
+        this.oDOM.btnBorrar.on("click", this.actualizarVista.bind(this));
 
     }// fin de la funcion controller
 
     actualizarVista (oEvent) {
-        // si el evento lo ha disparado btnBorrar
         if (oEvent.currentTarget.id == "btnBorrar"){
-            this.oDOM.fNombre.value = '';
+            this.oDOM.fNombre[0].value = '';
         }
-        this.oDOM.spanNombre.innerHTML = this.oDOM.fNombre.value;
-        console.log (this.oDOM.fNombre.value);
+        this.oDOM.spanNombre.innerHTML = this.oDOM.fNombre[0].value;
+        console.log (this.oDOM.fNombre[0].value);
     }// fin de la funcion actualizarVista
 }
